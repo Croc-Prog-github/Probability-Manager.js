7749c16eba4d88de43be0701c28b370443fd33ad
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-06-18 22:15:16 +0200

    1 file edited

--- core/Probability-Manager.ts
+++ core/Probability-Manager.ts
@@ -1,11 +1,14 @@
 declare module "https://raw.githubusercontent.com/Croc-Prog-github/Probability-Manager.js/main/core/Probability-Manager.js" {
+  type ProbabilityType = number | 'auto_DirectProp' | 'auto_InversProp';
+  
   class ProbabilityManager {
     constructor();
     addList(instanceName: string, listName: string): void;
-    addObject(instanceName: string, listName: string, object: string, probability: number): void;
-    getRandomObject(instanceName: string, listName: string): string;
+    addObject(instanceName: string, listName: string, object: string, probability: ProbabilityType): void;
+    getRandomObject(instanceName: string, listName: string): string | number;
     clearInstance(instanceName: string): void;
     clearAll(): void;
   }
+  
   export default ProbabilityManager;
 }
