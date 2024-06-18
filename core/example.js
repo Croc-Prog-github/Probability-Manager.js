9bb5c587af0218a67c35186e66635e88be4529b5
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-06-18 19:25:50 +0200

    2 file edited

--- core/example.js
+++ core/example.js
@@ -1,20 +1,32 @@
-import probManager from "https://raw.githubusercontent.com/Croc-Prog-github/Probability-Manager.js/main/core/Probability-Manager.js";
+const probManager = new ProbabilityManager();
 
 // Creazione di istanze e liste
 probManager.addList('Istanza1', '1');
 probManager.addList('Istanza1', '2');
 
 // Aggiunta di oggetti con probabilità
-probManager.addObject('Istanza1', '1', 'Moneta d\'oro', 50);
-probManager.addObject('Istanza1', '1', 'Moneta d\'argento', 50);
-probManager.addObject('Istanza1', '2', 'Moneta di bronzo', 100);
+try {
+  probManager.addObject('Istanza1', '1', 'Moneta d\'oro', 50);
+  probManager.addObject('Istanza1', '1', 'Moneta d\'argento', 50);
+  probManager.addObject('Istanza1', '2', 'Moneta di bronzo', 100);
+} catch (error) {
+  console.error(error.message);
+}
 
 // Estrazione di un oggetto casuale
-let randomEvent = probManager.getRandomObject('Istanza1', '1');
-console.log(`Oggetto estratto: `+ randomEvent);
+try {
+  let randomEvent = probManager.getRandomObject('Istanza1', '1');
+  console.log(`Oggetto estratto: ${randomEvent}`);
 
-randomEvent = probManager.getRandomObject('Istanza1', '2');
-console.log(`Oggetto estratto: `+ randomEvent);
+  randomEvent = probManager.getRandomObject('Istanza1', '2');
+  console.log(`Oggetto estratto: ${randomEvent}`);
+} catch (error) {
+  console.error(error.message);
+}
 
 // Pulizia delle istanze
-probManager.clearInstance('Istanza1');
+try {
+  probManager.clearInstance('Istanza1');
+} catch (error) {
+  console.error(error.message);
+}
